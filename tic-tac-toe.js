7e2e2668986795e38736639b72110fae89186e87
Lloyd Bill <lloydbill.lariosa@gmail.com>
2024-06-11 16:02:26 +0800

    added checkWin function to check pattern wins

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,18 @@
-
 let currentMark = "O";
+
+
+const checkWin = (mark) => {
+  const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]              // Diagonals
+  ];
+
+
+  return winPatterns.some(pattern =>
+    pattern.every(index => cells[index].innerText === mark)
+  );
+};
 
 function createPlayer(name, mark) {
 
@@ -42,7 +55,11 @@ cells.forEach(cell => {
       mark.classList.add("mark");
       cell.appendChild(mark);
       mark.innerText = currentMark;
+
+      if (checkWin(currentMark)) {
+        console.log(`${currentMark} wins`);
+        // You can add code here to display a message or take other actions
+      }
     }
   });
 })
-
