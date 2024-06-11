65e72b5a4217f9574e651e631f03c90a7ba5fbf3
Lloyd Bill <lloydbill.lariosa@gmail.com>
2024-06-11 18:54:31 +0800

    made iffes and privatizes global variables

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,65 +1,110 @@
-let currentMark = "O";
 
 
-const checkWin = (mark) => {
-  const winPatterns = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-    [0, 4, 8], [2, 4, 6]              // Diagonals
-  ];
 
+const Gameboard = (() => {
 
+  const cells = document.querySelectorAll('.cell');
+
+  const getCells = () => cells;
+
+  const checkWin = (mark) => {
+    const winPatterns = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+      [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
+  
+  
   return winPatterns.some(pattern =>
-    pattern.every(index => cells[index].innerText === mark)
-  );
-};
+      pattern.every(index => cells[index].innerText === mark)
+    );
+  };
 
-function createPlayer(name, mark) {
 
-  const playerName = name;
 
-  const playerMark = mark; 
 
-  const playerMove = function() {
-    currentMark = playerMark;
+  return {checkWin, getCells};
+
+
+
+})();
+
+
+const GameController = (() => {
+
+  let currentMark = "O";
+
+  const createPlayer = (name, mark) => {
+    const playerName = name;
+    const playerMark = mark; 
+    const playerMove = function() {
+      currentMark = playerMark;
+    }
+    return {playerName,playerMark, playerMove}; 
+  
+  
   }
 
 
-  return {playerName,playerMark, playerMove}; 
-
-
-}
-
-
-
-const player1 = createPlayer("lloyd","X");
-
-const player2 = createPlayer("bill","O");
+  const player1 = createPlayer("lloyd","X");
+  const player2 = createPlayer("bill","O");
 
 
 
-const cells = document.querySelectorAll('.cell');
+  const startGame = () => {
 
-cells.forEach(cell => {
-  cell.addEventListener("click", () => {
+    Gameboard.getCells().forEach(cell => {
+      cell.addEventListener("click", () => {
+      
+        if (cell.innerText === "") {
+          if (currentMark === "O") {
+            player1.playerMove();
+          
+          } else if ( currentMark === "X") {
+            player2.playerMove();
+          }
   
-    if (cell.innerText === "") {
-      if (currentMark === "O") {
-        player1.playerMove();
-       
-      } else if ( currentMark === "X") {
-        player2.playerMove();
-      }
+          const mark = document.createElement("h1");
+          mark.classList.add("mark");
+          cell.appendChild(mark);
+          mark.innerText = currentMark;
+  
+          if (Gameboard.checkWin(currentMark)) {
+            console.log(`${currentMark} wins`);
+            // You can add code here to display a message or take other actions
+          }
+        }
+      });
+    })
+  }
+  
 
-      const mark = document.createElement("h1");
-      mark.classList.add("mark");
-      cell.appendChild(mark);
-      mark.innerText = currentMark;
 
-      if (checkWin(currentMark)) {
-        console.log(`${currentMark} wins`);
-        // You can add code here to display a message or take other actions
-      }
-    }
-  });
-})
+
+  return {createPlayer, startGame};
+
+
+})();
+
+
+
+GameController.startGame();
+
+
+  
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
